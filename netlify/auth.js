fd168084819a69dd78ff5bb7a616fa9fd8204f53
juan ojeda <juan@juanojeda.com>
2018-09-28 15:00:35 +1000

    amending structure of auth src file

--- netlify/auth.js
+++ netlify/auth.js
@@ -1,12 +1,14 @@
-export default function auth(event, context, callback) {
-  const { PASS } = context.headers;
-  const passAttempt = event.body.pass;
-  const respTemplate = {
-    statusCode: 401,
-  };
-  if (PASS === passAttempt) {
-    callback(null, Object.assign({}, respTemplate, { statusCode: 200 }));
-  } else {
-    callback(null, Object.assign({}, respTemplate));
-  }
-}
+export default {
+  handler: function auth(event, context, callback) {
+    const { PASS } = context.headers;
+    const passAttempt = event.body.pass;
+    const respTemplate = {
+      statusCode: 401,
+    };
+    if (PASS === passAttempt) {
+      callback(null, Object.assign({}, respTemplate, { statusCode: 200 }));
+    } else {
+      callback(null, Object.assign({}, respTemplate));
+    }
+  },
+};
