eb9a9d0975c2748d87c4f013c5d3c351c70de46d
juan ojeda <juan@juanojeda.com>
2018-09-28 15:06:59 +1000

    amend lambda to sit in exports.handler

--- netlify/auth.js
+++ netlify/auth.js
@@ -1,14 +1,14 @@
-export default {
-  handler: function auth(event, context, callback) {
-    const { PASS } = context.headers;
-    const passAttempt = event.body.pass;
-    const respTemplate = {
-      statusCode: 401,
-    };
-    if (PASS === passAttempt) {
-      callback(null, Object.assign({}, respTemplate, { statusCode: 200 }));
-    } else {
-      callback(null, Object.assign({}, respTemplate));
-    }
-  },
+const handler = function handler(event, context, callback) {
+  const { PASS } = context.headers;
+  const passAttempt = event.body.pass;
+  const respTemplate = {
+    statusCode: 401,
+  };
+  if (PASS === passAttempt) {
+    callback(null, Object.assign({}, respTemplate, { statusCode: 200 }));
+  } else {
+    callback(null, Object.assign({}, respTemplate));
+  }
 };
+
+export { handler };
