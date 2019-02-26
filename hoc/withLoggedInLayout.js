af43c179198b074c399856205353b61e5b5aa6a9
juan ojeda <juan@juanojeda.com>
2019-02-26 23:23:00 +1100

    add Navigation component

--- hoc/withLoggedInLayout.js
+++ hoc/withLoggedInLayout.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { GridContainer, GridCell } from '../components/Grid';
 import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
 
-const WithLoggedInLayout = ({ children, className }) => (
+const WithLoggedInLayout = ({ children, className, navKey }) => (
   <GridContainer {...{ className }}>
     <GridCell md={4} lg={4}>
       <Logo />
+      <Navigation activePage={navKey} />
     </GridCell>
     <GridCell md={8} lg={8}>
       {children}
