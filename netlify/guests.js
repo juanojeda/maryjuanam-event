66b9268680456a5a15e0f26224763054bef27b0f
juan ojeda <juan@juanojeda.com>
2019-06-04 21:52:31 +1000

    remove dependency on token file, read firebase token from env vars

---
+++ netlify/guests.js
@@ -0,0 +1,21 @@
+import firebaseDB from '../utils/firebase';
+
+const handler = async function handler(event, context) {
+  try {
+    const guests = [];
+    const guestsDB = await firebaseDB.collection('guests').get();
+
+    guestsDB.forEach(doc => guests.push(doc.data()));
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(guests),
+      headers: { 'Content-Type': 'application/json' },
+    };
+  } catch (err) {
+    console.log('Error getting documents', err);
+    return err;
+  }
+};
+
+export { handler };
---
+++ utils/firebase.js
@@ -0,0 +1,23 @@
+import admin from 'firebase-admin';
+import env from './getEnv';
+
+const firebaseKey = {
+  type: env.FIREBASE_ACC_TYPE,
+  project_id: env.FIREBASE_PROJECT_ID,
+  private_key_id: env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: env.FIREBASE_CLIENT_EMAIL,
+  client_id: env.FIREBASE_CLIENT_ID,
+  auth_uri: env.FIREBASE_AUTH_URI,
+  token_uri: env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: env.FIREBASE_CLIENT_X509_CERT_URL,
+};
+
+admin.initializeApp({
+  credential: admin.credential.cert(firebaseKey),
+});
+
+const firebaseDB = admin.firestore();
+
+export default firebaseDB;
