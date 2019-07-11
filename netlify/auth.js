31a219378410775ab4c0fcec3a2e0b0a9cb27ad1
juan ojeda <juan@juanojeda.com>
2019-07-11 21:22:34 +1000

    make password easier to get

--- netlify/auth.js
+++ netlify/auth.js
@@ -2,7 +2,7 @@ const handler = function handler(event, context, callback) {
   const { PASS } = process.env;
   const { pass } = JSON.parse(event.body);
 
-  if (PASS === pass) {
+  if (PASS.toLowerCase() === pass.toLowerCase()) {
     callback(null, { statusCode: 200, body: 'password is correct' });
   } else {
     callback(null, { statusCode: 401, body: 'password is incorrect' });
